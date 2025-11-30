301eb2343e4da6d1e9f97a35da410d0eb22c5369
gaurav <gauravsinharnc@gmail.com>
2025-11-30 23:59:41 +0530

    apply .gitignore and remove class files

---
+++ src/Arrays/SortedArray.java
@@ -0,0 +1,21 @@
+class Solution{
+    public static boolean check(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%nums.length]){
+                count++;
+            }
+        }
+        if(count>1){
+            return false;
+        }
+        return true;
+    }
+    
+    public static void main(String[] args){
+        int[] nums={3,4,5,1,2};
+        boolean result=check(nums);
+        System.out.println(result);
+
+    }
+}
